d21c1a2726d4934184c5de2353beb3e18661e079
Eduarda <dudamagesk@gmail.com>
2026-03-31 15:48:12 -0300

    utils

---
+++ train_pipeline.py
@@ -0,0 +1,204 @@
+
+######################################################################################
+# Configuring importing libraries
+######################################################################################
+from utils.loader import get_data_loader
+from utils.train import fit_model
+from utils.eval import test_model
+from utils.data_algumentation import ImgTrainTransform, ImgEvalTransform
+
+import os
+import json
+import time
+import torch
+import numpy as np
+import pandas as pd
+import torch.nn as nn
+import torch.optim as optim
+
+from sacred import Experiment
+# from models_hub import set_class_model
+from sacred.observers import FileStorageObserver
+from sentence_transformers import SentenceTransformer
+######################################################################################
+
+######################################################################################
+# Variables and configurations
+######################################################################################
+
+# Getting the local configurations
+CLASS_TYPE = "diag" # triage or diag
+with open("./config.json") as json_file:
+    _LOCAL_CONFIG = json.load(json_file)
+_DATASET_BASE_PATH = _LOCAL_CONFIG["dataset_folder_path"]
+_CSV_PATH_TRAIN = os.path.join(_DATASET_BASE_PATH, "pad-ufes-25-diag_folders_raw.csv")
+_JSON_PATH_TRAIN = os.path.join(_DATASET_BASE_PATH, "anamnese_diag_raw.json")
+_IMGS_FOLDER_TRAIN = os.path.join(_DATASET_BASE_PATH, "images")
+
+if CLASS_TYPE == "triage":
+    _CSV_PATH_TRAIN = os.path.join(_DATASET_BASE_PATH, "pad-ufes-25-triage_folders_raw.csv")
+    _JSON_PATH_TRAIN = os.path.join(_DATASET_BASE_PATH, "anamnese_triage_raw.json")
+
+# Avoiding the tokenizers warning
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
+
+TARGET_COLUMN = "clinicalMacroCID"
+TARGET_NUMBER_COLUMN = "diagnostic-number"
+IMG_COLUMN = "img-id"
+
+# Starting sacred experiment
+ex = Experiment()
+
+######################################################################################
+
+@ex.config
+def cnfg():
+
+    # Defines the folder to be used as validation
+    _folder = 1
+
+    # Models configurations
+    _use_meta_data = True
+    _neurons_reducer_block = 0
+    _comb_method = "metablock" # metanet, concat, or metablock
+    _comb_config = [64, 768] # number of metadata
+    _batch_size = 30
+    _epochs = 50
+
+    _llm_type = "small" # small
+    _model_name = 'resnet-50'
+    _save_folder = f"results/{CLASS_TYPE}_{_model_name}_{_comb_method}SE_V2_FITZ__{_llm_type}_folder_{str(_folder)}_{str(time.time()).replace('.', '')}"
+
+    # Training variables
+    _best_metric = "loss"
+    _pretrained = True
+    _optmizer = "adam" # adam or sgd
+    _lr_init = 0.0001
+    _sched_factor = 0.1
+    _sched_min_lr = 1e-6
+    _sched_patience = 10
+    _early_stop = 15
+    _metric_early_stop = None
+    _weights = "frequency"       
+    
+
+    # This is used to configure the sacred storage observer. In brief, it says to sacred to save its stuffs in
+    # _save_folder. You don't need to worry about that.
+    SACRED_OBSERVER = FileStorageObserver(_save_folder)
+    ex.observers.append(SACRED_OBSERVER)
+
+@ex.automain
+def main (_folder, _lr_init, _sched_factor, _sched_min_lr, _sched_patience, _batch_size, _epochs, 
+          _early_stop, _weights, _model_name, _pretrained, _optmizer, _save_folder, _best_metric, _llm_type,
+          _neurons_reducer_block, _comb_method, _comb_config, _use_meta_data, _metric_early_stop):
+    
+
+    _metric_options = {
+        'save_all_path': os.path.join(_save_folder, "best_metrics"),
+        'pred_name_scores': 'predictions_best_test.csv',
+        'normalize_conf_matrix': True}
+    _checkpoint_best = os.path.join(_save_folder, 'best-checkpoint/best-checkpoint.pth')
+
+    # Loading the csv file
+    csv_all_folders = pd.read_csv(_CSV_PATH_TRAIN)
+    
+    meta_json = sentence_model = None
+    if _use_meta_data:
+        with open(_JSON_PATH_TRAIN) as json_file:
+            meta_json = json.load(json_file)
+
+        if _llm_type == "small":            
+            sentence_model = SentenceTransformer("sentence-transformers/paraphrase-albert-small-v2")
+        elif _llm_type == "large":
+            sentence_model = SentenceTransformer("sentence-transformers/all-mpnet-base-v2")        
+        else:
+            raise ValueError("Invalid LLM type")
+
+    print("-" * 50)
+    print("- Loading validation data...")
+    val_csv_folder = csv_all_folders[ (csv_all_folders['folder'] == _folder) ]
+    train_csv_folder = csv_all_folders[ csv_all_folders['folder'] != _folder ]
+
+    # Loading validation data
+    val_imgs_id = val_csv_folder[IMG_COLUMN].values
+    val_imgs_path = ["{}/{}".format(_IMGS_FOLDER_TRAIN, img_id) for img_id in val_imgs_id]
+    val_labels = val_csv_folder[TARGET_NUMBER_COLUMN].values
+    val_meta_data = list()
+
+    if _use_meta_data:
+        for img_id in val_imgs_id:
+            doc_vec = sentence_model.encode(meta_json[img_id], show_progress_bar=False)            
+            val_meta_data.append(doc_vec)   
+        val_meta_data = np.asarray(val_meta_data)
+        print(f"-- Using {val_meta_data.shape} meta-data features")        
+    else:
+        print("-- No metadata")
+        val_meta_data = None
+    val_data_loader = get_data_loader (val_imgs_path, val_labels, val_meta_data, transform=ImgEvalTransform(),
+                                       batch_size=_batch_size, shuf=True, num_workers=16, pin_memory=True)
+    print("-- Validation partition loaded with {} images".format(len(val_data_loader)*_batch_size))
+  
+  
+    print("- Loading training data...")
+    train_imgs_id = train_csv_folder[IMG_COLUMN].values
+    train_imgs_path = ["{}/{}".format(_IMGS_FOLDER_TRAIN, img_id) for img_id in train_imgs_id]
+    train_labels = train_csv_folder[TARGET_NUMBER_COLUMN].values
+    train_meta_data = list()
+    if _use_meta_data:
+        for img_id in train_imgs_id:
+            doc_vec = sentence_model.encode(meta_json[img_id], show_progress_bar=False)           
+            train_meta_data.append(doc_vec)     
+        train_meta_data = np.asarray(train_meta_data)   
+        print(f"-- Using {doc_vec.shape} meta-data features")
+    else:
+        print("-- No metadata")
+        train_meta_data = None
+
+    train_data_loader = get_data_loader (train_imgs_path, train_labels, train_meta_data, transform=ImgTrainTransform(),
+                                       batch_size=_batch_size, shuf=True, num_workers=16, pin_memory=True)
+    print("-- Training partition loaded with {} images".format(len(train_data_loader)*_batch_size))
+    print("-"*50)
+
+    ####################################################################################################################
+
+    ser_lab_freq = train_csv_folder.groupby([TARGET_COLUMN])[IMG_COLUMN].count()
+    _labels_name = ser_lab_freq.index.values
+    _freq = ser_lab_freq.values
+    print(ser_lab_freq)
+
+    ####################################################################################################################
+    print("- Loading", _model_name)
+
+    model = set_class_model(_model_name, len(_labels_name), neurons_reducer_block=_neurons_reducer_block,
+                      comb_method=_comb_method, comb_config=_comb_config, pretrained=_pretrained)
+    ####################################################################################################################
+    if _weights == 'frequency':
+        _weights = (_freq.sum() / _freq).round(3)
+
+    loss_fn = nn.CrossEntropyLoss(weight=torch.Tensor(_weights).cuda())
+
+    if _optmizer == "adam":
+        optimizer = optim.Adam(model.parameters(), lr=_lr_init)
+    elif _optmizer == "sgd":        
+        optimizer = optim.SGD(model.parameters(), lr=_lr_init, momentum=0.9, weight_decay=0.001)
+    else:
+        raise ValueError("Invalid optimizer name")
+    
+    scheduler_lr = optim.lr_scheduler.ReduceLROnPlateau(optimizer, factor=_sched_factor, min_lr=_sched_min_lr,
+                                                                    patience=_sched_patience)
+    ####################################################################################################################
+
+    print("- Starting the training phase...")
+    print("-" * 50)
+    fit_model (model, train_data_loader, val_data_loader, optimizer=optimizer, loss_fn=loss_fn, epochs=_epochs,
+               epochs_early_stop=_early_stop, save_folder=_save_folder, initial_model=None, metric_early_stop=_metric_early_stop,
+               device=None, schedule_lr=scheduler_lr, config_bot=None, model_name="CNN", resume_train=False,
+               history_plot=True, val_metrics=["balanced_accuracy"], best_metric=_best_metric)
+    ####################################################################################################################
+
+    # Testing the validation partition
+    print("- Evaluating the validation partition...")
+    test_model (model, val_data_loader, checkpoint_path=_checkpoint_best, loss_fn=loss_fn, save_pred=True,
+                partition_name='eval', metrics_to_comp='all', class_names=_labels_name, metrics_options=_metric_options,
+                apply_softmax=True, verbose=False)
+    ####################################################################################################################
---
+++ utils/eval.py
@@ -0,0 +1,313 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Author: André Pacheco
+E-mail: pacheco.comp@gmail.com
+
+This file function to evaluate a model
+
+If you find any bug or have some suggestion, please, email me.
+"""
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as nnF
+from .metrics import Metrics, accuracy
+from .checkpoints import load_model
+from .utils.classification_metrics import AVGMetrics
+from tqdm import tqdm
+
+
+def metrics_for_eval (model, data_loader, device, loss_fn, topk=2, get_balanced_acc=False, get_auc=False):
+    """
+        This function returns accuracy, topk accuracy and loss for the evaluation partition
+
+        :param model (nn.Model): the model you' want to evaluate
+        :param data_loader (DataLoader): the DataLoader containing the data partition
+        :param checkpoint_path(string, optional): string with a checkpoint to load the model. If None, none checkpoint is
+        loaded. Default is None.
+        :param loss_fn (nn.Loss): the loss function used in the training
+        :param get_balanced_acc (bool, optional); if you want to compute the balanced accuracy for the eval partition
+        set it as True. The default is False because it may impact in the training phase.
+        :param get_auc (bool, optional); if you want to compute the AUC for the eval partition
+        set it as True. The default is False because it may impact in the training phase.
+        :return: a instance of the classe metrics
+    """
+
+    # setting the model to evaluation mode
+    model.eval()
+
+    print ("\nEvaluating...")
+    # Setting tqdm to show some information on the screen
+    with tqdm(total=len(data_loader), ascii=True, ncols=100) as t:
+
+        # Setting require_grad=False in order to dimiss the gradient computation in the graph
+        with torch.no_grad():
+
+            loss_avg = AVGMetrics()
+            acc_avg = AVGMetrics()
+            topk_avg = AVGMetrics()
+
+            opt_metrics = list()
+            if not get_balanced_acc and not get_auc:
+                metrics = None
+            else:                
+                if get_balanced_acc:
+                    opt_metrics.append('balanced_accuracy')
+                if get_auc:
+                    opt_metrics.append('auc')                    
+                metrics = Metrics(opt_metrics)                
+
+
+            for data in data_loader:
+
+                # In data we may have imgs, labels and extra info. If extra info is [], it means we don't have it
+                # for the this training case. Imgs came in data[0], labels in data[1] and extra info in data[2]
+                try:
+                    images_batch, labels_batch, meta_data_batch,_ , _ = data
+                except ValueError:
+                    images_batch, labels_batch = data
+                    meta_data_batch = []
+                except:
+                    break
+
+                if len(meta_data_batch):
+                    # Moving the data to the deviced that we set above
+                    images_batch, labels_batch = images_batch.to(device), labels_batch.to(device)
+                    meta_data_batch = meta_data_batch.to(device)
+                    meta_data_batch = meta_data_batch.float()
+
+                    # Doing the forward pass using meta_data
+                    pred_batch = model(images_batch, meta_data_batch)
+                else:
+                    # Moving the data to the device that we set above
+                    images_batch, labels_batch = images_batch.to(device), labels_batch.to(device)
+
+                    # Doing the forward pass without using meta-data
+                    pred_batch = model(images_batch)
+
+                # Computing the loss
+                L = loss_fn(pred_batch, labels_batch)
+                # Computing the accuracy
+                acc, topk_acc = accuracy(pred_batch, labels_batch, topk=(1, topk))
+
+                loss_avg.update(L.item())
+                acc_avg.update(acc.item())
+                topk_avg.update(topk_acc.item())
+
+                if metrics is not None:
+                    # Moving the data to CPU and converting it to numpy in order to compute the metrics
+                    pred_batch_np = nnF.softmax(pred_batch, dim=1).cpu().numpy()
+                    labels_batch_np = labels_batch.cpu().numpy()
+                    # updating the scores
+                    metrics.update_scores(labels_batch_np, pred_batch_np)
+
+                # Updating tqdm
+                t.set_postfix(loss='{:05.3f}'.format(loss_avg()))
+                t.update()
+
+    bal_acc, auc = None, None
+    if metrics is not None:
+        # Getting the metrics
+        metrics.compute_metrics()
+        try:
+            bal_acc =  metrics.metrics_values['balanced_accuracy']
+        except KeyError:
+            bal_acc = None        
+        try:
+            auc =  metrics.metrics_values['auc']
+        except KeyError:
+            auc = None
+
+    return {"loss": loss_avg(), "accuracy": acc_avg(), "topk_acc": topk_avg(), "balanced_accuracy": bal_acc, 'auc': auc}
+
+
+# Testing the model
+def test_model (model, data_loader, checkpoint_path=None, loss_fn=None, device=None, save_pred=False,
+                    partition_name='Test', metrics_to_comp=('accuracy'), class_names=None, metrics_options=None,
+                    apply_softmax=True, verbose=True, full_path_pred=None):
+    """
+    This function evaluates a given model for a given data_loader
+
+    :param model (nn.Model): the model you'd like to evaluate
+    :param data_loader (DataLoader): the DataLoader containing the data partition
+    :param checkpoint_path(string, optional): string with a checkpoint to load the model. If None, no checkpoint is
+    loaded. Default is None.
+    :param loss_fn (nn.Loss): the loss function used in the training
+    :param partition_name (string): the partition name
+    :param metrics (list, tuple or string): it's the variable that receives the metrics you'd like to compute.
+        IMPORTANT: if metrics is None, only the prediction.csv will be generated. Default is only the accuracy.
+    :param class_names (list, tuple): a list or tuple containing the classes names in the same order you use in the
+        label. For ex: ['C1', 'C2']. For more information about the options, please, refers to
+        jedy.pytorch.model.metrics.py
+    :param metrics_ options (dict): this is a dict containing some options to compute the metrics. Default is None.
+    For more information about the options, please, refers to jedy.pytorch.model.metrics.py
+    :param device (torch.device, optional): the device to use. If None, the code will look for a device. Default is
+    None. For more information about the options, please, refers to metrics.py
+    :param verbose (bool, optional): if you'd like to print information o the screen. Default is True
+    True. Default is False.
+
+    :return: a instance of the classe metrics
+    """
+
+    # setting the model to evaluation mode
+    model.eval()
+
+    def _get_predictions (model, images_batch, meta_data_batch=None):        
+        with torch.no_grad():
+            if meta_data_batch is None:
+                pred_batch = model(images_batch)
+            else:
+                pred_batch = model(images_batch, meta_data_batch)
+        return pred_batch
+
+    if checkpoint_path is not None:
+        model = load_model(checkpoint_path, model)
+
+    if device is None:
+        # Setting the device
+        if torch.cuda.is_available():
+            device = torch.device("cuda:" + str(torch.cuda.current_device()))
+        else:
+            device = torch.device("cpu")
+
+    # Moving the model to the given device
+    model.to(device)
+
+    if loss_fn is None:
+        loss_fn = nn.CrossEntropyLoss()
+
+    # Setting the metrics object
+    metrics = Metrics (metrics_to_comp, class_names, metrics_options)
+
+    print("Testing...")
+    # Setting tqdm to show some information on the screen
+    with tqdm(total=len(data_loader), ascii=True, ncols=100) as t:
+
+        loss_avg = AVGMetrics()
+
+        for data in data_loader:
+            # In data we may have images, labels, meta-data, and image ID. If meta-data is [], it means we don't have it
+            # for the this training case. Images came in data[0], labels in data[1], meta_data in data[2], and image_id
+            # in data[3]
+            try:
+                images_batch, labels_batch, meta_data_batch, img_id, extra_data = data
+            except ValueError:
+                images_batch, labels_batch = data
+                meta_data_batch = []
+                img_id = None
+            except:
+                break
+
+            if len(meta_data_batch):
+                # Moving the data to the device that we set above
+                images_batch = images_batch.to(device)
+                if len(labels_batch):
+                    labels_batch = labels_batch.to(device)
+                meta_data_batch = meta_data_batch.to(device)
+                meta_data_batch = meta_data_batch.float()
+
+                # Doing the forward pass using meta-data
+                pred_batch = _get_predictions (model, images_batch, meta_data_batch)
+            elif len(labels_batch):
+                # Moving the data to the device that we set above
+                images_batch, labels_batch = images_batch.to(device), labels_batch.to(device)
+
+                # Doing the forward pass without using meta-data
+                pred_batch = _get_predictions(model, images_batch)
+            else:
+                # Moving the data to the deviced that we set above
+                images_batch = images_batch.to(device)
+
+                # Doing the forward pass without using meta_data
+                pred_batch = _get_predictions(model, images_batch)
+
+            # Computing the loss
+            if len(labels_batch):
+                L = loss_fn(pred_batch, labels_batch)
+                loss_avg.update(L.item())
+                labels_batch_np = labels_batch.cpu().numpy()
+            else:
+                labels_batch_np = None
+                loss_avg.update(0)
+
+            # Moving the data to CPU and converting it to numpy in order to compute the metrics
+            if apply_softmax:
+                pred_batch_np = nnF.softmax(pred_batch,dim=1).cpu().numpy()
+            else:
+                pred_batch_np = pred_batch.cpu().numpy()
+
+            # updating the scores
+            metrics.update_scores(labels_batch_np, pred_batch_np, img_id, extra_data)
+
+            # Updating tqdm
+            if metrics.metrics_names is None:
+                t.set_postfix(loss='{:05.3f}'.format(0.0))
+            else:
+                t.set_postfix(loss='{:05.3f}'.format(loss_avg()))
+
+            t.update()
+
+    # Adding loss into the metric values
+    metrics.add_metric_value("loss", loss_avg())
+
+    # Getting the metrics
+    metrics.compute_metrics(group_by_extra_data=True)
+
+    if save_pred or metrics.metrics_names is None:
+        if full_path_pred is None:
+            metrics.save_scores()
+        else:
+            _spt = full_path_pred.split('/')
+            _folder = "/".join(_spt[0:-1])
+            _p = _spt[-1]
+            metrics.save_scores(folder_path=_folder, pred_name=_p)
+
+    if verbose:
+        print('- {} metrics:'.format(partition_name))
+        metrics.print()
+
+
+    return metrics.metrics_values
+
+
+def test_single_input(model, trans, img, meta_data=None, apply_softmax=True):
+    """
+    This function returns the predictions of model for a single input image and metadata (if applicable).
+
+    :param model (nn.Model): the loaded model that will output the predictions
+    :param trans (torchvision.transforms.Compose): transform operations to be carry out on the images
+    :param img (PIL.Image): the image that will feed the model
+    :param meta_data (np.array, list, None): the metadata that will feed the model. If None, it assumes there is no
+                                             metadata.
+    :param apply_softmax (bool): set it as True if want to perform the softmax operation on the predictions
+    """
+
+    # setting the model to evaluation mode
+    model.eval()
+
+    img = trans(img).unsqueeze(0) # adding the batch size dimension (which must be 1)
+    if meta_data is not None:
+        meta_data = np.asarray(meta_data)
+        meta_data = torch.from_numpy(meta_data).unsqueeze(0) # idem
+
+    with torch.no_grad():
+
+        if meta_data is not None:
+            pred = model(img, meta_data)
+        else:
+            pred = model(img)
+
+    if apply_softmax:
+        pred = nnF.softmax(pred, dim=1)
+
+    return pred.numpy()
+
+
+
+
+        
+
+
+
